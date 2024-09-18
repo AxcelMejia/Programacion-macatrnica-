1f5f98d7658905ee5668968c95e7eea7794c7ce5
AxcelMejia <lecxe16@gmail.com>
2024-09-18 17:46:18 -0400

    Segundo commit

---
+++ 2.2/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/* incremento de precio.
+El programa, al recibir como dato el precio de un producto importado, incrementa 11% el mismo si esta es inferior a $1,500.
+PRE y NPR: variable de tipo real*/
+
+void main(void)
+{
+    float PRE, NPR;
+    printf("infrese el precio del producto: ");
+    scanf("%f", &PRE);
+}
